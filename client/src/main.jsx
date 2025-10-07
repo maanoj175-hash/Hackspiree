0a4bbab845894d7fe0c428220ef74e2af8625826
maanoj175-hash <maanoj175@gmail.com>
2025-10-08 00:36:38 +0530

    removed file

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import { createRoot } from "react-dom/client";
-import RootRouter from "./router.jsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-  <RootRouter />
-);
